e247651ea7553b62ec2cf4abcbea6b24533d3209
bnhfn <amine.benhafoun@gmail.com>
2023-03-17 13:05:39 -0700

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - A program that prints all single digit number.
+ * Description: Use only putchar.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 0; ch <= 9; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
