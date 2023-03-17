04fb34f25baf837922c33b26e0c00021ad90fbc6
bnhfn <amine.benhafoun@gmail.com>
2023-03-17 15:29:19 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main()
 {
 char hex_digits[] = "0123456789abcdef";
 int i;
-for (i = 0; i < 16; i++) 
-{	
-putchar(hex_digits[i]);  
+for (i = 0; i < 16; i++)
+{
+putchar(hex_digits[i]);
 }
 putchar('\n');
 return 0;
