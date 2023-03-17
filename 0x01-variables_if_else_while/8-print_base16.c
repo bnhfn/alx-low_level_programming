c9b846ca420f0a36da58bdbd23931d8c19158637
bnhfn <amine.benhafoun@gmail.com>
2023-03-17 15:27:55 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,13 +4,14 @@
  * Description: Use putchar twice.
  * Return: Always 0 (Success)
 */
-int main(void)
+int main()
 {
+char hex_digits[] = "0123456789abcdef";
 int i;
-for (i = 0; i <= 15; i++)
-{
-putchar('%xi + 0');
+for (i = 0; i < 16; i++) 
+{	
+putchar(hex_digits[i]);  
 }
 putchar('\n');
-return (0);
+return 0;
 }
