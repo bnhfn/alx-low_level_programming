5bc59610b6f098039c525dc823e319317d999544
bnhfn <amine.benhafoun@gmail.com>
2023-03-17 09:45:03 -0700

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main - A program that prints a line with puts function
+ * Description: Prints out a string to stdout.
+ * Return: Always 0 (Success)
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
