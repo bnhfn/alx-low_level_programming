669532f6358c15337cbec1f6bac159b9b67c8e12
bnhfn <amine.benhafoun@gmail.com>
2023-03-17 12:03:04 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase.
+ * Description: Use only putchar.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+return (0);
+}
