798d06aaf569bc2089e929c55e16edbd5b79f937
bnhfn <amine.benhafoun@gmail.com>
2023-03-17 13:18:35 -0700

    digit using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - A program that prints all single digit number.
+ * Description: Use printf.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 0; ch <= 9; ch++)
+putchar("%d", ch);
+putchar("\n");
+return (0);
+}
