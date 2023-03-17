3510cc0d5ee609e87853e1e7d299905c89546090
bnhfn <amine.benhafoun@gmail.com>
2023-03-17 15:13:52 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - A program that prints all single digit number.
+ * Description: Use putchar twice.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 15; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
