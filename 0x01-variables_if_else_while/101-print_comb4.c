ed4da10d3b1f0ffbf62f4d12dd10eef9a8fa86e6
bnhfn <amine.benhafoun@gmail.com>
2023-03-20 13:12:04 -0700

    comb of 3 different digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,18 +6,18 @@
 int main(void)
 {
 int n, m, l;
-for (n = 48; n <= 56; n++)
+for (n = 48; n <= 58; n++)
 {
-for (m = 49; m <= 57; m++)
+for (m = 49; m <= 58; m++)
 {
-for (l = 49; l <= 57; l++)
+for (l = 50; l <= 58; l++)
 {
 if (l > m && m > n)
 {
 putchar(n);
 putchar(m);
 putchar(l);
-if (n != 56 || m != 57 || l != 57)
+if (n != 56 || m != 57 || l != 58)
 {
 putchar(',');
 putchar(' ');
