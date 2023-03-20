3d1b5732a213facee757112197b340b95ffc9e10
bnhfn <amine.benhafoun@gmail.com>
2023-03-20 12:24:59 -0700

    comb of 2 different digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
+ * main - prints all possible different combinations of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m, l;
-
-	for (n = 48; n < 58; n++)
-	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int n, m;
+for (n = 48; n < 57; n++)
+{
+for (m = 49; m < 58; m++)
+{
+if (m > n)
+{
+putchar(n);
+putchar(m);
+if (n != 57 || m != 58)
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
