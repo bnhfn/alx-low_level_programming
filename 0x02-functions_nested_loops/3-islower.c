5a34d22216889c26b1ffe67794747917079d9b4b
bnhfn <amine.benhafoun@gmail.com>
2023-03-21 13:14:54 -0700

    if c islowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * islowercase - Function
+ * Description: checks if the passing alphabet is lowercase
+ * Return: 1 if true || 0 if false
+ * @c: store an integer value
+*/
+
+int _islower(int c);
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
