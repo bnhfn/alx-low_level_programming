01fc7038fa127c30aff2b61101538951e7cbff4c
bnhfn <amine.benhafoun@gmail.com>
2023-03-21 14:25:08 -0700

    if if else else

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Function
+ * Description: prints the sign of a number
+ * Return: 1 if n>0 || 0 n=0 || -1 if n>0
+ * @c: store an integer value
+*/
+
+int print_sign(int n)
+{
+if (n >= 1)
+{
+printf("\+");
+return (1);
+}
+else if (n == 0)
+{
+printf("0");
+return (0);
+}
+else
+{
+printf("-1");
+return (0);
+}
+}
