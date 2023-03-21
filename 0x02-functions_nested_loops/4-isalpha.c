30ee8859aeb118d2e041ca338fa8f213b2612b65
bnhfn <amine.benhafoun@gmail.com>
2023-03-21 13:57:28 -0700

    if else lower & upper alphabt

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - Function
+ * Description: check if the passing alphabet is lowercase or uppercase
+ * Return: 1 if true || 0 if false
+ * @c: store an integer value
+*/
+
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+{
+else if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
