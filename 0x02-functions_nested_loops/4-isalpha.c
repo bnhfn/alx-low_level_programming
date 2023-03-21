8fa7e84e9375360bedc458ffe15e01a5d87066a0
bnhfn <amine.benhafoun@gmail.com>
2023-03-21 14:03:18 -0700

    if else lower & upper alphabt

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,17 +9,19 @@
 
 int _isalpha(int c)
 {
-if (c >= 97 && c <= 122)
-{
-return (1);
-}
-{
-if else (c >= 65 && c <= 90)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	{
+		else if (c >= 65 && c <= 90)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 }
