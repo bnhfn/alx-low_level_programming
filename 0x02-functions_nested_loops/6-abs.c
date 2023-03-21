ee17aa6e6de974e54d568f84287306842ae58d22
bnhfn <amine.benhafoun@gmail.com>
2023-03-21 15:01:09 -0700

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Function
+ * Description: return abs value of a given number
+ * Return: always 0 (success)
+ * @int: store an integer value
+*/
+
+int _abs(int)
+{
+int x, y;
+y = abs(x);
+
+_putchar(y);
+
+return (0);
+}
