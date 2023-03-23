1275c1f4d8a18881118285c522ceb5c136181901
bnhfn <amine.benhafoun@gmail.com>
2023-03-23 10:17:52 -0700

    function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,15 @@
  * description: function that prints the numbers, from 0 to 9,
  * followed by a new line.
  * @i: store an integer value
- * return: o (success)
+ * return: void
  **/
 
 void print_numbers(void)
 {
-int i;
+char i;
 for (i = '0'; i <= '9'; i++)
-	_putchar(i);
-return (0);
+{
+_putchar(i);
+}
+_putchar("\n");
 }
