a03d5c07f3a6ba5fec52e8de19408bda2759b431
bnhfn <amine.benhafoun@gmail.com>
2023-03-27 15:18:59 -0700

    pointer to an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,15 @@
 #include "main.h"
 /**
  * reset_to_98 - function
- * description: function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * description: function that takes a pointer to an int
+ * parameter and updates the value it points to to 98
  *parameter: @int
- *
+ * @n: store an integer value
+ *return: always (0) succes
  **/
 void reset_to_98(int *n)
 {
 	int n = 98;
-	int* p =  
+	int p = *n;
+return (0);
+}
