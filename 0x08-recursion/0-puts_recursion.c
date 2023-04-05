b5fc67ac8b78650e25daef054aa3271c60e34810
bnhfn <amine.benhafoun@gmail.com>
2023-04-05 15:20:00 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdout.h>
+
+/**
+ * _puts_recursion - function that prints a recursion
+ * @s: input
+ * return: always 0 success
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar("\n")
+}
