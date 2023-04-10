22731d95664bc447227847803df42e8ee27fc400
bnhfn <amine.benhafoun@gmail.com>
2023-04-10 15:41:38 -0700

    program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
